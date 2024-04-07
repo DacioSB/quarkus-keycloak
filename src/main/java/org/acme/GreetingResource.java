5fd28bf765405942cc0ef62c0a25b89416aecc0c
DacioSB <dacio.bezerra@ccc.ufcg.edu.br>
2024-04-07 20:45:22 -0300

    :necktie: update business logic adding secured route

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,8 @@
 package org.acme;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,8 +11,23 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "db.username")
+    String username;
+
+    @ConfigProperty(name = "db.password")
+    String password;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Path("db")
+    @RolesAllowed("admin")
+    public String db() {
+        return username + ":" + password;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed("user")
     public String hello() {
         return "Hello from RESTEasy Reactive";
     }
